33ee1021ac0e3f0edc0a566103c5978e6dd1029c
erwan-blancard <erwan.blancard@laplateforme.io>
2023-04-05 12:22:49 +0200

    commit job 1

---
+++ job01/job01.ino
@@ -0,0 +1,15 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+
+void setup() {
+  // put your setup code here, to run once:
+  lcd.begin(16, 2);
+  lcd.clear();
+  lcd.write("Hello World");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
